f60ba47616358817e0028fb5fe6710db25402f2c
Yang Shaoyue <yangshaoyueas@gmail.com>
2015-07-03 21:48:18 +0800

    no path but through

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /*
  * 触发动画函数，以显示文章
  */
+var smallScreen = window.innerWidth < 900;
+var footer, copyright, c_font;
 function animationTrigger() {
 	$("#avatar a:nth-child(2)").remove();
 	$("#avatar").addClass("avatar-animation");
@@ -13,21 +15,29 @@ function animationTrigger() {
 
 	$("#container").fadeIn(1000);
 
-	var footer = document.getElementById("footer");
-	var copyright = document.createElement("span");
-	var c_font = document.createElement("i");
-
-	if (!c_font.getAttribute("class")) {
-		c_font.setAttribute("class", "fa fa-copyright");
-	}
-
-	copyright.appendChild(c_font);
-	footer.appendChild(copyright);
-
 	$("footer").addClass("footer-animation1");
-	$("footer #footer span").click(function() {
-		$("footer").toggleClass("footer-animation2");
-	});
+
+	if (!smallScreen) {
+		footer = document.getElementById("footer");
+		copyright = document.createElement("span");
+		if (!copyright.getAttribute("class")) {
+			copyright.setAttribute("class", "cr-font");
+		}
+
+		c_font = document.createElement("i");
+
+		if (!c_font.getAttribute("class")) {
+			c_font.setAttribute("class", "fa fa-copyright");
+		}
+
+		copyright.appendChild(c_font);
+		footer.appendChild(copyright);
+		
+		$("footer #footer span.cr-font").click(function() {
+			$("footer").toggleClass("footer-animation2");
+		});
+	}
+	
 
 	$("#avatar a").unbind("mouseenter");
 	$("#avatar a").unbind("mouseleave");
@@ -61,7 +71,7 @@ $(document).ready(function() {
 /*
  * 点击导航栏时如果未触发动画，则先触发动画
  */
-function wasTriggered() {
+(function wasTriggered() {
 	var navBar = document.getElementById("nav-bar"),
 		navUl = navBar.getElementsByTagName("ul")[0],
 		navLi = navUl.getElementsByTagName("li");
@@ -75,8 +85,8 @@ function wasTriggered() {
 			}, true);
 		}
 	}
-}
-wasTriggered();
+})();
+// wasTriggered();
 
 function paginatorModify() {
 	// 首页/分类页的分页按钮添加pjax功能
@@ -103,7 +113,6 @@ function paginatorModify() {
 	// 将向前向后按钮文本设置为空 
 	$("nav#page-nav .extend").text("");
 
-
 	$("nav#page-nav .extend").append("<i></i>");
 	$("nav#page-nav .extend").append("<i></i>");
 }
@@ -123,9 +132,67 @@ function afterPjax() {
 	});
 }
 
+/*
+ * 屏幕尺寸小于900px
+ */
+
+var smallScreenHeight = window.innerHeight;
+
+// 侧边栏伸缩
+function toggleClassStretch() {
+	$("header #toggle").toggleClass("stretch2");
+	$("header #avatar").toggleClass("stretch2");
+	$("header nav#nav-bar").toggleClass("stretch");
+	$("#author h2").toggleClass("stretch");
+	$("header .social-wrapper").toggleClass("stretch");
+	$("header .separator").toggleClass("stretch");
+	$("footer.footer-animation1").toggleClass("stretch");
+	$("body").toggleClass("overflow-hidden");
+}
+
+function toggleSidebar() {
+	$("header #toggle").one("mouseenter", function() { // 一次性事件
+		$(this).addClass("stretch1");
+		$("header nav#nav-bar").addClass("stretch");
+		$("header #avatar").addClass("stretch1");
+		$("body").addClass("overflow-hidden");		
+		animationTrigger();
+		paginatorModify();
+
+		$("header .separator").animate({left:"2.5rem"}, 500);
+
+		$("#footer .copyright span.power").remove();
+		$("#footer .copyright a:first-child").remove();
+		
+	});
+
+	$("header #toggle").click(function() {
+		toggleClassStretch();
+	});
+
+	$("nav#nav-bar ul li a").click(function() {
+		toggleClassStretch();
+	});
+}
+
+if (smallScreen) {
+	$("nav#nav-bar").css('height', smallScreenHeight);
+
+	var $sep = "<div class='separator'></div>";
+	$("header div.nav-wrapper").append($sep);
+
+	toggleSidebar();
+}
+
+// pjax
 $(document).pjax('a[data-pjax]', '#container', { fragment: '#container', timeout: 10000 });
 $(document).on({
+	'pjax:start': function() {
+		$("#container").fadeOut(500);
+	},
+
 	'pjax:end': function() {
+		$("#container").fadeIn(500);
 		paginatorModify();
 		afterPjax();
 	}
